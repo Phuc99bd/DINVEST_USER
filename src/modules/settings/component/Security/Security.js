a6214d673c13eb86218c8d899cc040c68e902660
Long Dep Trai Vcl <letrunglong07@gmail.com>
2020-12-23 21:27:25 +0700

    setting updates

--- src/modules/settings/component/Security/Security.js
+++ src/modules/settings/component/Security/Security.js
@@ -1,14 +1,29 @@
 import { Input, Button } from "antd";
 import React from "react";
+import { Authy } from "./Authy/Authy";
+import { useState } from "react";
 
 const Security = () => {
+  const [isShowAuth, setShowAuth] = useState(false);
+  const onClick = () => {
+    setShowAuth(!isShowAuth);
+    // console.log(isShowAuth);
+  };
+  const showAuth = () => {
+    if (isShowAuth == true) {
+      return <Authy />;
+    }
+  };
   return (
     <div className="security parent">
       <div className="title">Choose security type</div>
       <div className="content">
         <Button className="email">Email</Button>
-        <Button className="authy">Authy</Button>
+        <Button className="authy" onClick={onClick}>
+          Authy
+        </Button>
       </div>
+      {showAuth()}
     </div>
   );
 };
